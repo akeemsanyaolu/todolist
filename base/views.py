e79f27b780c01ac10ac954fa7d5d1f189eb0df52
Akeem Sanyaolu <akeemsanyaolu2@gmail.com>
2023-07-20 01:50:35 +0100

    Login functionality

--- base/views.py
+++ base/views.py
@@ -1,3 +1,6 @@
+from typing import Any, Dict
+from django.forms.models import BaseModelForm
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from .models import Task
@@ -24,6 +27,12 @@ class TaskList(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = 'tasks'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = context['task'].filter(user=self.request.user)
+        context['count'] = context['count'].filter(complete=False).count()
+        return context
+
 class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task
     context_object_name = 'task'
@@ -33,6 +42,10 @@ class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
     fields = '__all__'
     success_url  = reverse_lazy('tasks')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(TaskCreate, self).form_valid(form)
 
 class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
