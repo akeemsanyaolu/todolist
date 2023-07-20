75516d41b87a97d6239e74c1a66e228068476eab
Akeem Sanyaolu <akeemsanyaolu2@gmail.com>
2023-07-20 02:00:09 +0100

    register functionality

--- base/views.py
+++ base/views.py
@@ -29,8 +29,8 @@ class TaskList(LoginRequiredMixin, ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['tasks'] = context['task'].filter(user=self.request.user)
-        context['count'] = context['count'].filter(complete=False).count()
+        context['tasks'] = context['tasks'].filter(user=self.request.user)
+        context['count'] = context['tasks'].filter(complete=False).count()
         return context
 
 class TaskDetail(LoginRequiredMixin, DetailView):
@@ -40,7 +40,7 @@ class TaskDetail(LoginRequiredMixin, DetailView):
 
 class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
-    fields = '__all__'
+    fields = ['title', 'description', 'complete']
     success_url  = reverse_lazy('tasks')
 
     def form_valid(self, form):
@@ -49,7 +49,7 @@ class TaskCreate(LoginRequiredMixin, CreateView):
 
 class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
-    fields = '__all__'
+    fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('tasks')
 
 class TaskDelete(LoginRequiredMixin, DeleteView):
